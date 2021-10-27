304a015582772092349ae5785e88c8cf8c3628fa
shivam danawale <shivamdanawale@gmail.com>
2021-10-27 23:47:27 +0530

    Update Login.jsx

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -5,8 +5,8 @@ function Login() {
   return (
     <>
       <div className="container flex items-center justify-center border-red-500 bg-red-50 h-screen">
-        <div className="text-center w-1/3 rounded-2xl p-3">
-          <h2 className="text-4xl m-4">Welcome to devcord!</h2>
+        <div className="text-center w-1/3 shadow-2xl rounded-2xl p-3">
+          <h2 className="text-4xl m-4">Welcome to Devcord!</h2>
           <p className="m-3">Log in with your email and password </p>
           <form className="flex flex-col text-center justify-center items-center">
             <input
