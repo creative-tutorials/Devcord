6df2abdb4b8fa68a65f64856b03518b05ddf2738
shivam danawale <shivamdanawale@gmail.com>
2021-10-27 18:24:35 +0530

    fist commit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import {store,persistor} from "./ReduxStore";
+import { store, persistor } from "./ReduxStore";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 ReactDOM.render(
@@ -17,7 +17,4 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
